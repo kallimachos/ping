9ea405db9bae2e28197f4a7efc1dec5138c32849
kallimachos <kallimachos@gmail.com>
2019-12-11 15:47:24 +1000

    Format output

--- ping/downtime.py
+++ ping/downtime.py
@@ -18,13 +18,15 @@ def diff(Fail, Pass):
 def downtime():
     """Calculate the total downtime."""
     filepath = path.join(Path(__file__).absolute().parent, "results.txt")
+    fails = 0
     time = timedelta()
     with open(filepath, "r") as f:
         for line in f:
             Fail = line
             Pass = next(f)
+            fails += 1
             time += diff(Fail, Pass)
-    return time
+    return (fails, time)
 
 
 def uptime(downtime):
@@ -36,7 +38,7 @@ def uptime(downtime):
 
 
 if __name__ == "__main__":
-    downtime = downtime()
+    fails, downtime = downtime()
     today, days, uptime = uptime(downtime)
-    print(f"\n{'From':<15}{'To':<15}{'Days':<10}{'Downtime':<30}Uptime")
-    print(f"{'2019-11-15':<15}{today:<15}{days:<10}{str(downtime):<30}{uptime}%\n")
+    print(f"\n{'From':<15}{'To':<15}{'Days':<10}{'Fails':<10}{'Downtime':<30}Uptime")
+    print(f"{'2019-11-15':<15}{today:<15}{days:<10}{fails:<10}{str(downtime):<30}{uptime}%\n")
