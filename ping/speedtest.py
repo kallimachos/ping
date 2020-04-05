5c5c56e544ae8923b8b7a8e53f5e9998449c4055
kallimachos <kallimachos@gmail.com>
2020-04-06 07:13:14 +1000

    Add timer

--- ping/speedtest.py
+++ ping/speedtest.py
@@ -3,6 +3,7 @@
 """Run speedtest-cli and print result."""
 
 import subprocess
+from codetiming import Timer
 from yaspin import yaspin
 
 
@@ -19,7 +20,8 @@ def speedtest():
 
 
 if __name__ == "__main__":
-    with yaspin(text=f"Running speedtest", color="green") as spinner:
-        response = speedtest()
-        spinner.ok()
-    print(f"\n{response}")
+    with Timer(text="\033[32mElapsed time: {:0.2f} seconds\033[0m\n"):
+        with yaspin(text=f"Running speedtest", color="green") as spinner:
+            response = speedtest()
+            spinner.ok()
+        print(f"\n{response}")
